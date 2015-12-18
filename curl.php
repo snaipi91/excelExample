cd451dc2c6dc24e809ec66125638c7baa174bcd6
snaipi91 <snaipi91@rambler.ru>
2015-12-19 03:30:33 +0400

    Парсер

---
+++ curl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 16.12.2015
+ * Time: 2:31
+ */
+header("Content-Type:application/vnd.ms-excel");
+header("Content-Disposition:attachment;filename='test.xls'");
+
+$token = "d56d138a-ee63-80d2-2fc5d4ed2f5b";
+
+// делаем запрос к API Football
+$ch = curl_init("http://football-api.com/api/?Action=fixtures&OutputType=JSON&APIKey=" . $token . "&from_date=19.07.2015&to_date=15.12.2015");
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$obj = curl_exec($ch);
+
+curl_close($ch);
+
+$obj = json_decode($obj, true);
+$player = $obj['matches'];
+
+require('excel.php');
+
+?>
+
+<pre>
+    <? echo print_r($player); ?>
+</pre>
---
+++ excel.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 18.12.2015
+ * Time: 1:51
+ */
+
+require_once 'Classes/PHPExcel.php';
+
+$objPHPExcel = new PHPExcel();
+
+/* Настройки */
+$objPHPExcel->setActiveSheetIndex(0);
+$active_sheet = $objPHPExcel->getActiveSheet();
+$active_sheet->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT);
+$active_sheet->getPageSetup()->SetPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
+
+$active_sheet->setTitle("Футбольная АПЛ");
+
+$active_sheet->getColumnDimension('A')->setWidth(12);
+$active_sheet->getColumnDimension('B')->setWidth(12);
+$active_sheet->getColumnDimension('C')->setWidth(15);
+$active_sheet->getColumnDimension('D')->setWidth(15);
+$active_sheet->getColumnDimension('E')->setWidth(10);
+
+$active_sheet->mergeCells('A1:C1');
+$active_sheet->setCellValue('A1', 'API Football');
+$active_sheet->setCellValue('A2','Match date');
+$active_sheet->setCellValue('B2','Match time');
+$active_sheet->setCellValue('C2','Home team');
+$active_sheet->setCellValue('D2','Guest team');
+$active_sheet->setCellValue('E2','Account');
+
+//массив стилей
+$style_header = array(
+    //Шрифт
+    'font'=>array(
+        'bold' => true,
+        'name' => 'Times New Roman',
+        'size' => 12
+    ),
+    //Заполнение цветом
+    'fill' => array(
+        'type' => PHPExcel_STYLE_FILL::FILL_SOLID,
+        'color'=>array(
+            'rgb' => 'CCC'
+        )
+    )
+);
+
+$active_sheet->getStyle('A2:E2')->applyFromArray($style_header);
+
+
+$row_start = 3;
+$i = 0;
+
+// Заполняем ячейки данными
+foreach($player as $item) {
+
+    $row_next = $row_start + $i;
+
+    $active_sheet->setCellValue('A'.$row_next, $item['match_formatted_date']);
+    $active_sheet->setCellValue('B'.$row_next, $item['match_time']);
+    $active_sheet->setCellValue('C'.$row_next, $item['match_localteam_name']);
+    $active_sheet->setCellValue('D'.$row_next, $item['match_visitorteam_name']);
+    $active_sheet->setCellValue('E'.$row_next, $item['match_ft_score']);
+
+    $i++;
+}
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+$objWriter->save('php://output');
